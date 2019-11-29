03caf775fbc8658facae3e3a6c62cfa6d97152f8
Keyhan Vakil <kvakil@berkeley.edu>
2019-11-29 01:08:36 +0000

    Suppress dd output in copy script.

--- copy-to-sector.sh
+++ copy-to-sector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sector_no=$(($3 * 18))
-dd if=/dev/zero of="$2" count=1 bs=512 seek="$sector_no" conv=notrunc
-dd if="$1"      of="$2" count=1 bs=512 seek="$sector_no" conv=notrunc
+dd if=/dev/zero of="$2" count=1 bs=512 seek="$sector_no" conv=notrunc 2>/dev/null
+dd if="$1"      of="$2" count=1 bs=512 seek="$sector_no" conv=notrunc 2>/dev/null
